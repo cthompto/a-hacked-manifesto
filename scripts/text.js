12e42cb7d08869f23301b797ef53b68b892805d8
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2024-03-09 23:26:11 -0500

    establish basic random recall from json

--- scripts/text.js
+++ scripts/text.js
@@ -1,16 +1,22 @@
 var manifesto;
 fetch('./text/hacker-text.json')
   .then(response => response.json())
-  .then(data => console.log(data))
-  .then(data => manifesto = data)
+  .then(data => {
+    console.log(data);
+    manifesto = data;
+})
   .catch(error => console.error('Error fetching JSON:', error));
 
-console.log(manifesto);
+  //console.log(manifesto);
 
 const ranBut = document.getElementById("rb");
 ranBut.addEventListener("click", randomText);
 
 function randomText() {
-    let ranSec = manifesto;
-    console.log("hi");
+    let ranSec = Math.floor(Math.random() * ((Object.keys(manifesto.sections).length)-1));
+    let ranPar = Math.floor(Math.random() * (Object.keys(manifesto.sections[ranSec].paragraphs).length));
+    let ranLin = Math.floor(Math.random() * (Object.keys(manifesto.sections[ranSec].paragraphs[ranPar].sentences).length));
+    console.log("Section: " + manifesto.sections[ranSec].name);
+    console.log("Paragraph: " + manifesto.sections[ranSec].paragraphs[ranPar].number);
+    console.log("Line: " + manifesto.sections[ranSec].paragraphs[ranPar].sentences[ranLin]);
 }
